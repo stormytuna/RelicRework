e6dfbce371475ea35d5ded04741a7d24bceb78ba
stormytuna <stormytuna@outlook.com>
2023-03-16 17:23:29 +0000

    Small refactors

--- src/main/java/relicrework/patches/relics/CeramicFish_Patch.java
+++ src/main/java/relicrework/patches/relics/CeramicFish_Patch.java
@@ -18,31 +18,23 @@ public class CeramicFish_Patch {
     private static final RelicStrings relicStrings = CardCrawlGame.languagePack.getRelicStrings("CeramicFish");
 
     @SpirePatch(clz = CeramicFish.class, method = "onObtainCard")
-    public static class CeramicFish_PrefixOnObtainCard {
+    public static class CeramicFish_RemoveOnObtainCard {
         @SpirePrefixPatch
         public static SpireReturn patch(CeramicFish __instance, AbstractCard card) {
-            if (RelicRework.changeCeramicFish) {
-                return SpireReturn.Return();
-            }
-
-            return SpireReturn.Continue();
+            return RelicRework.changeCeramicFish ? SpireReturn.Return() : SpireReturn.Continue();
         }
     }
 
     @SpirePatch(clz = CeramicFish.class, method = "getUpdatedDescription")
-    public static class CeramicFish_PrefixGetUpdatedDescription {
+    public static class CeramicFish_ReplaceGetUpdatedDescription {
         @SpirePrefixPatch
         public static SpireReturn<String> patch(CeramicFish __instance) {
-            if (RelicRework.changeCeramicFish) {
-                return SpireReturn.Return(relicStrings.DESCRIPTIONS[0]);
-            }
-
-            return SpireReturn.Continue();
+            return RelicRework.changeCeramicFish ? SpireReturn.Return(relicStrings.DESCRIPTIONS[0]) : SpireReturn.Continue();
         }
     }
 
     @SpirePatch(clz = ShopScreen.class, method = "init")
-    public static class ShopScreen_PostfixInit {
+    public static class ShopScreen_ApplyDiscount {
         @SpirePostfixPatch
         public static void patch(ShopScreen __instance, ArrayList<AbstractCard> coloredCards, ArrayList<AbstractCard> colorlessCards) {
             if (RelicRework.changeCeramicFish) {
