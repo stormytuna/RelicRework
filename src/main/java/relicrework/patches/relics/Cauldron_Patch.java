5383ecb8ce5eba1678150925ed92f6603e27b125
stormytuna <stormytuna@outlook.com>
2023-03-24 17:03:01 +0000

    Change Cauldron

--- src/main/java/relicrework/patches/relics/Cauldron_Patch.java
+++ src/main/java/relicrework/patches/relics/Cauldron_Patch.java
@@ -1,12 +1,11 @@
 package relicrework.patches.relics;
 
 import com.evacipated.cardcrawl.modthespire.lib.*;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.PotionHelper;
 import com.megacrit.cardcrawl.localization.RelicStrings;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
-import com.megacrit.cardcrawl.potions.PotionSlot;
 import com.megacrit.cardcrawl.relics.Cauldron;
 import com.megacrit.cardcrawl.rewards.RewardItem;
 import relicrework.RelicRework;
@@ -14,7 +13,8 @@ import relicrework.util.GeneralUtils;
 
 public class Cauldron_Patch {
     private static final RelicStrings RELIC_STRINGS = CardCrawlGame.languagePack.getRelicStrings(Cauldron.ID);
-    private static final int NUM_POTIONS = 3;
+    private static final int POTION_COMMON_CHANCE = 45;
+    private static final int POTION_UNCOMMON_CHANCE = 30;
 
     @SpirePatch(clz = Cauldron.class, method = "onEquip")
     public static class Cauldron_ReplaceOnEquip {
@@ -42,12 +42,24 @@ public class Cauldron_Patch {
                 }
             }
 
-            // Grant potion slots
-            AbstractPlayer player = AbstractDungeon.player;
-            player.potionSlots += 1;
-            player.potions.add(new PotionSlot(player.potionSlots - 1));
-
             return SpireReturn.Return();
+        }
+    }
+
+    @SpirePatch(clz = AbstractDungeon.class, method = "returnRandomPotion", paramtypez = { boolean.class })
+    public static class AbstractDungeon_ReplaceReturnRandomPotion {
+        @SpirePrefixPatch
+        public static SpireReturn<AbstractPotion> patch(boolean limited) {
+            if (AbstractDungeon.player.hasRelic(Cauldron.ID) && RelicRework.isEnabled(Cauldron.ID)) {
+                int roll = AbstractDungeon.potionRng.random(0, 99);
+                if (roll < PotionHelper.POTION_COMMON_CHANCE)
+                    return SpireReturn.Return(AbstractDungeon.returnRandomPotion(AbstractPotion.PotionRarity.COMMON, limited));
+                if (roll < PotionHelper.POTION_UNCOMMON_CHANCE + PotionHelper.POTION_COMMON_CHANCE)
+                    return SpireReturn.Return(AbstractDungeon.returnRandomPotion(AbstractPotion.PotionRarity.UNCOMMON, limited));
+                return SpireReturn.Return(AbstractDungeon.returnRandomPotion(AbstractPotion.PotionRarity.RARE, limited));
+            }
+
+            return SpireReturn.Continue();
         }
     }
 }
